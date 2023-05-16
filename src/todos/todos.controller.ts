60c2b85f4a3789918d0764ac15f0bc2f2741e4f4
papyntown <realistedem@gmail.com>
2023-05-16 11:36:43 +0200

    typage

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { TodosService } from './todos.service';
+import { Todo } from './interface/todo.interface';
 
 // il Ecoute localhost:3000/todos
 @Controller('todos')
 export class TodosController {
   constructor(private readonly TodosService: TodosService) {}
   @Get()
-  findAllTodos(): any[] {
+  findAllTodos(): Todo[] {
     return this.TodosService.findAllTodos();
   }
 }
