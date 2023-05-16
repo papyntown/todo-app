f925e3ccdc259c119c185d47e97c437588991286
papyntown <realistedem@gmail.com>
2023-05-16 11:36:48 +0200

    typage

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -6,11 +6,13 @@ export class TodosService {
     {
       id: 1,
       title: 'Todo 1',
+      done: false,
       description: 'Todo 1 description',
     },
     {
       id: 2,
       title: 'Todo 2',
+      done: true,
       description: 'Todo 2 description',
     },
   ];
